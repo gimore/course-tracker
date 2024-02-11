9214b67004a700a6f2c974278f0a80e9d7af57e5
gimore <86805518+gimore@users.noreply.github.com>
2024-02-10 20:28:26 -0500

    Se agrega módulos de microservicios ms-gateway, ms-eureka y ms-config  sobre el proyecto

---
+++ ms-gateway/src/test/java/co/com/gimore/courses/gateway/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.gimore.courses.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
